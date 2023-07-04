118408e7b87d622b5597d12889c5f3526b6ea869
Firdaus Adib <i@firdaus.my>
2023-07-05 03:10:40 +0800

    aider: Print each row of the dataframe to the console in the `pages/video_list.py` file.
    # Context:
    USER: in the page `video_list.py`, display the dataframe as the data fetch, row by row
    ASSISTANT: To assist you better, I need to see the current content of the `video_list.py` file. Could you please share the *read-write* `video_list.py` file?
    USER: in the page `pages/video_list.py`, display the dataframe as the data fetch, row by row
    ASSISTANT: To assist you better, I need to see the current content of the `pages/video_list.py` file. Could you please share the *read-write* `pages/video_list.py` file?
    USER: import pandas as pd
    import scrapetube
    from datetime import datetime
    
    def fetch_videos(channel_id):
        try:
            existing_df = pd.read_csv('video_id.csv')
            existing_ids = existing_df['Video ID'].tolist()
        except FileNotFoundError:
            existing_ids = []
    
        videos = scrapetube.get_channel(channel_id)
        data = []
        for video in videos:
            vid_id = video['videoId']
            if vid_id in existing_ids:
                continue
            publishedTimeText = video["publishedTimeText"]["simpleText"]
            duration = video["lengthText"]["simpleText"]
            viewship = video["viewCountText"]["simpleText"].replace(",", "").split()[0]
            title = video["title"]["runs"][0]["text"]
            description = video["descriptionSnippet"]["runs"][0]["text"]
            created_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
            data.append([channel_id, created_date, vid_id, publishedTimeText, duration, viewship, title, description])
            print(vid_id)
        df = pd.DataFrame(data, columns=['Channel ID', 'Created Date', 'Video ID', 'Published Time', 'Duration', 'Viewship', 'Title', 'Description'])
        df.to_csv('video_id.csv', index=False, mode='a')
    
    with open('urls.txt', 'r') as f:
        for line in f:
            channel_id = line.strip().split(',')[2]
            fetch_videos(channel_id)
    
    ASSISTANT: To display the dataframe row by row, you can use the `iterrows()` function in pandas. This function returns an iterator yielding index and row data for each row.
    
    Here are the changes you need to make:
    
    ```python
    pages/video_list.py
    <<<<<<< ORIGINAL
        df = pd.DataFrame(data, columns=['Channel ID', 'Created Date', 'Video ID', 'Published Time', 'Duration', 'Viewship', 'Title', 'Description'])
        df.to_csv('video_id.csv', index=False, mode='a')
    =======
        df = pd.DataFrame(data, columns=['Channel ID', 'Created Date', 'Video ID', 'Published Time', 'Duration', 'Viewship', 'Title', 'Description'])
        for index, row in df.iterrows():
            print(row)
        df.to_csv('video_id.csv', index=False, mode='a')
    >>>>>>> UPDATED
    ```
    
    This will print each row of the dataframe to the console. If you want to format the output or only print certain columns, you can modify the print statement accordingly.

---
+++ pages/video_list.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import scrapetube
+from datetime import datetime
+
+def fetch_videos(channel_id):
+    try:
+        existing_df = pd.read_csv('video_id.csv')
+        existing_ids = existing_df['Video ID'].tolist()
+    except FileNotFoundError:
+        existing_ids = []
+
+    videos = scrapetube.get_channel(channel_id)
+    data = []
+    for video in videos:
+        vid_id = video['videoId']
+        if vid_id in existing_ids:
+            continue
+        publishedTimeText = video["publishedTimeText"]["simpleText"]
+        duration = video["lengthText"]["simpleText"]
+        viewship = video["viewCountText"]["simpleText"].replace(",", "").split()[0]
+        title = video["title"]["runs"][0]["text"]
+        description = video["descriptionSnippet"]["runs"][0]["text"]
+        created_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        data.append([channel_id, created_date, vid_id, publishedTimeText, duration, viewship, title, description])
+        print(vid_id)
+    df = pd.DataFrame(data, columns=['Channel ID', 'Created Date', 'Video ID', 'Published Time', 'Duration', 'Viewship', 'Title', 'Description'])
+    for index, row in df.iterrows():
+        print(row)
+    df.to_csv('video_id.csv', index=False, mode='a')
+
+with open('urls.txt', 'r') as f:
+    for line in f:
+        channel_id = line.strip().split(',')[2]
+        fetch_videos(channel_id)
