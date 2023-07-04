f8268530d4e92b0dbe10becf410c6247f0929fca
Firdaus Adib <i@firdaus.my>
2023-07-05 02:04:44 +0800

    Revised display_url in table view

--- app.py
+++ app.py
@@ -5,17 +5,23 @@ def save_url(url):
     with open('urls.txt', 'a') as f:
         f.write(url + '\n')
 
+import pandas as pd
+
 def display_urls():
     with open('urls.txt', 'r') as f:
-        urls = f.read()
-    st.write(urls)
+        data = [line.strip().split(',') for line in f]
+    df = pd.DataFrame(data, columns=['URL', 'Channel ID'])
+    st.table(df)
 
 st.title('YouTube Channel/Playlist Registration')
 
 url = st.text_input('Enter the YouTube channel or playlist URL')
 if st.button('Register'):
     channel_id = get_channel_id(url)
-    save_url(channel_id)
-    st.success('URL registered successfully')
+    if channel_id is not None:
+        save_url(f"{url}, {channel_id}")
+        st.success('URL registered successfully')
+    else:
+        st.error('Could not find channel ID')
 
 display_urls()
--- utils.py
+++ utils.py
@@ -1,8 +1,31 @@
+import googleapiclient.discovery
 import re
 
 def get_channel_id(url):
-    pattern = r'((?<=(v|V)/)|(?<=be/)|(?<=(\?|\&)v=)|(?<=embed/))([\w-]+)'
-    match = re.search(pattern, url)
+    # Extract the channel name from the URL using a regular expression
+    match = re.search(r'/user/([^/]+)', url)
     if match:
-        return match.group()
-    return None
+        channel_name = match.group(1)
+    else:
+        match = re.search(r'/channel/([^/]+)', url)
+        if match:
+            channel_name = match.group(1)
+        else:
+            channel_name = url
+
+    # Call the YouTube Data API to search for the channel
+    youtube = googleapiclient.discovery.build("youtube", "v3", developerKey="AIzaSyCJ14NIQl3MQgwayvg2vSyV5Pj0E9wj-Zk")
+    request = youtube.search().list(
+        part="id",
+        q=channel_name,
+        type="channel",
+        maxResults=1
+    )
+    response = request.execute()
+
+    # Extract the channel ID from the search results
+    if len(response['items']) == 1:
+        # print(response)
+        return response['items'][0]['id']['channelId']
+    else:
+        return None
